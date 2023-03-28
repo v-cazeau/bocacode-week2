c3c4965f3ea97bf79b40109f92b9df26475e54fb
Véronie R. Cazeau <veronie.cazeau@gmail.com>
2023-03-28 08:48:36 -0400

    NEW homework

---
+++ wk2-homework/weekTwoDaySix.js
@@ -0,0 +1,26 @@
+//question ONE
+
+let myCar = {
+    brand : 'Toyata', 
+    make : 'Prius', 
+    color : 'red'
+}
+
+const {brand, make, color} = myCar
+console.log(myCar)
+
+//question TWO 
+
+numberOfHours = 5
+
+function hourConverter() {
+    const seconds = numberOfHours*3600
+    return seconds
+}
+
+console.log(hourConverter())
+
+//question THREE 
+//Practice making entity relationship diagrams by building one for a pet store, either on paper or on a website such as draw.io. Include a table for pets, owners, and services.
+
+console.log('test')
